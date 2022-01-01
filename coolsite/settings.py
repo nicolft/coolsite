4f3ff9fb5af60f81405be6ee69c48a7e6f46dcdf
Salt <nicolft@gmail.com>
2022-01-01 08:51:30 -0600

    coolsite: Move SECRET_KEY to environment variable to make repo public (:

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -12,15 +12,18 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'TODO'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
